6bd1da5d84d50f6fedf072154b16661829203f9c
unknown <jsonfullstacker.cse@gmail.com>
2024-03-08 22:21:20 +0530

    react concepts module

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import FractalTree from './Components/FractalTree';
+function App() {
+  return (
+    <div>
+      <FractalTree/>
+    </div>
+  );
+}
+
+export default App;
